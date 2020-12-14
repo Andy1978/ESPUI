683d9d70864f73724e7ce289a548dfd46a67a1ce
Daniel Betz <daniel.betz11@gmx.de>
2020-12-14 11:06:54 +0100

    Einfacher Node-Server, der die statischen Ressourcen ausliefert und den ersten Websocket-Endpoint simuliert

---
+++ server.js
@@ -0,0 +1,43 @@
+// npm install
+// node server.js
+// Browser -> http://localhost:3000/index.htm
+
+const express = require('express');
+const app = express();
+const port = 3000;
+var expressWs = require('express-ws')(app);
+
+// Static HTML/JS
+app.use(express.static('data'));
+
+// Websocket-Endpoint
+app.ws('/ws', function(ws, req) {
+    var data = {
+        type: 200, // UI_INITIAL_GUI
+        sliderContinuous: false,
+        controls: []
+    }
+
+    // Es gibt 19 Controls, identifiziert durch type: 0...18
+    // Die hier einfach mal an den Client schicken
+    for (var i = 0; i < 19; i++) {
+        // ID 10 ist leider kein UI-Element, sondern "ADD_GRAPH_POINT", deswegen überspringen
+        if (i === 10) continue;
+
+        data.controls.push({
+            id: 'id_' + i,
+            type: i,
+            label: 'Label for Ele ' + i,
+            value: 0,
+            color: 0, // C_TURQUOISE
+            parentControl: false, // irgendwas mit tabs vs rows
+            selected: false, // option-elemente preselecten
+        });
+    }
+
+    ws.send(JSON.stringify(data));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
